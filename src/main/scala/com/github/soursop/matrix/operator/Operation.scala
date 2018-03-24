65780ec181f1b40ae90364bf8ef5e8fbc177cead
stepping1st <stepping1st@gmail.com>
2018-03-25 01:13:25 +0900

    fix add transpose logic

--- src/main/scala/com/github/soursop/matrix/operator/Operation.scala
+++ src/main/scala/com/github/soursop/matrix/operator/Operation.scala
@@ -2,9 +2,14 @@ package com.github.soursop.matrix.operator
 
 object Operation {
 
-  trait Op
+  trait Op {
+    def unwrap(): Op = this
+  }
   type Copyable = {
     def copy(nodes: List[Op]): Ops
+  }
+  trait Reducer[T <: Op] {
+    def + (op: Op): T
   }
   object Ops {
     type Copyable = {
@@ -17,9 +22,11 @@ object Operation {
     def + (op: Op): Ops = op match {
       case some: Ops if (getClass.equals(some.getClass)) =>
         copy(nodes ::: some.asInstanceOf[Ops].nodes)
+      case Minus(_) => copy(nodes :+ op)
       case some: Ops if (some.nodes.length == 1) => copy(nodes :+ some.nodes.head)
       case _ => copy(nodes :+ op)
     }
+    override def unwrap(): Op = if (nodes.length == 1) nodes.head else this
   }
   case class Multiply(override val nodes: List[Op]) extends Ops(nodes)
   case class Divide(override val nodes: List[Op]) extends Ops(nodes)
@@ -36,27 +43,40 @@ object Operation {
 
 
   def parsing(text: String): Op = {
-    parsing("", Plus(Nil), 0, text)
+    parsing("", Plus(Nil), 0, 0, text).unwrap
   }
-
-  def parsing(buffer: String, ops: Ops, depth: Int, text: String): Op = {
-    if (text.isEmpty) ops + Terminal(buffer.trim)
-    else {
+  def parsing(buffer: String, ops: Ops, sign: Int, depth: Int, text: String): Op = {
+    def reduce(next: Ops): Op = {
+      if (depth == 0) parsing("", next, sign + 1, depth, text.tail)
+      else parsing(buffer + text.head, ops, sign + 1, depth, text.tail)
+    }
+    if (text.isEmpty) buffer.trim match {
+      case "" => ops
+      case trim if (trim.endsWith("'")) =>
+        ops + Transpose(if (sign > 0) parsing(trim.init) else Terminal(trim.init))
+      case trim =>
+        ops + (if (sign > 0) parsing(trim) else Terminal(trim))
+    } else {
       text.head match {
-        case '(' => parsing(buffer, ops, depth + 1, text.tail)
-        case ')' => parsing(buffer, ops, depth - 1, text.tail)
-        case '+' if depth == 0 =>
-          parsing("", Plus(Nil) + (ops + parsing(buffer)), depth, text.tail)
-        case '-' if depth == 0 =>
-          parsing("", Minus(Nil) + (ops + parsing(buffer)), depth, text.tail)
-        case c => parsing(buffer + c, ops, depth, text.tail)
+        case '(' => parsing(buffer, ops, sign, depth + 1, text.tail)
+        case ')' => parsing(buffer, ops, sign, depth - 1, text.tail)
+        case '/' => reduce(Divide(Nil) + (ops + parsing(buffer)))
+        case '*' => reduce(Multiply(Nil) + (ops + parsing(buffer)))
+        case '+' => reduce(Plus(Nil) + (ops + parsing(buffer)))
+        case '-'  =>
+          if (depth == 0) ops + parsing(buffer) + parsing("", Minus(Nil), sign + 1, depth, text.tail)
+          else parsing(buffer + text.head, ops, sign + 1, depth, text.tail)
+        case c => parsing(buffer + c, ops, sign, depth, text.tail)
       }
     }
   }
 
   def main(args: Array[String]): Unit = {
     // build
-    val query = "((2:3 * 3:2') + 4) + 2' - 2'"
+    val query = "- ((2:3 * 3:2') - 4) + 2' - 2'"
+//val query = "- ((2:3 * 3:2') - 4)"
+    //    val query = "- 4 + 2'"
+//    val query = "- 2' - 2'"
     val words = parsing(query)
 
     println(words)
