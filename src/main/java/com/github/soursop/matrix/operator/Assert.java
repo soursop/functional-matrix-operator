bf73500256132e712141184bb37d6837ccc1647a
stepping1st <stepping1st@gmail.com>
2018-04-08 22:21:23 +0900

    chore change format method

--- src/main/java/com/github/soursop/matrix/operator/Assert.java
+++ src/main/java/com/github/soursop/matrix/operator/Assert.java
@@ -1,21 +1,19 @@
 package com.github.soursop.matrix.operator;
 
-import static com.github.soursop.matrix.operator.Functions.asFormat;
-
 class Assert {
     static IllegalArgumentException throwIndexException(int height, int width, DoubleMatrix matrix) {
-        return new IllegalArgumentException(asFormat("Could't found value of index height:%d width:%d from %s", height, width, matrix.asSimple(0)));
+        return new IllegalArgumentException(String.format("Could't found value of index height:%d width:%d from %s", height, width, matrix.asSimple(0)));
     }
 
     static void assertElementSize(Sign sign, DoubleMatrix one, DoubleMatrix other) {
         if (one.height() != other.height() || one.width() != one.width()) {
-            throw new IllegalArgumentException(asFormat("Could't match %s of %s and %s", sign.sign, one.asSimple(0), other.asSimple(0)));
+            throw new IllegalArgumentException(String.format("Could't match %s of %s and %s", sign.sign, one.asSimple(0), other.asSimple(0)));
         }
     }
 
     static void assertProductSize(Sign sign, DoubleMatrix one, DoubleMatrix other) {
         if (one.width() != other.height()) {
-            throw new IllegalArgumentException(asFormat("Could't match %s of %s and %s", sign.sign, one.asSimple(0), other.asSimple(0)));
+            throw new IllegalArgumentException(String.format("Could't match %s of %s and %s", sign.sign, one.asSimple(0), other.asSimple(0)));
         }
     }
 
@@ -27,27 +25,27 @@ class Assert {
 
     static void assertOutOfIndex(int idx, int size) {
         if (size <= idx) {
-            throw new ArrayIndexOutOfBoundsException(asFormat("Could't found value of index:%d from size:%d", idx, size));
+            throw new ArrayIndexOutOfBoundsException(String.format("Could't found value of index:%d from size:%d", idx, size));
         }
     }
 
     static int assertSameHeightExceptZero(int one, int another) {
         if (one != 0 && another != 0 && one != another) {
-            throw new IllegalArgumentException(asFormat("Illegal matrix height size %d != %d", one, another));
+            throw new IllegalArgumentException(String.format("Illegal matrix height size %d != %d", one, another));
         }
         return Math.max(one, another);
     }
 
     static int assertSameWidthExceptZero(int one, int another) {
         if (one != 0 && another != 0 && one != another) {
-            throw new IllegalArgumentException(asFormat("Illegal matrix width size %d != %d", one, another));
+            throw new IllegalArgumentException(String.format("Illegal matrix width size %d != %d", one, another));
         }
         return Math.max(one, another);
     }
 
     static int assertHeight(int size, int width) {
         if (size % width != 0) {
-            throw new IllegalArgumentException(asFormat("Illegal matrix size %d / %d = %d", size, width, size % width));
+            throw new IllegalArgumentException(String.format("Illegal matrix size %d / %d = %d", size, width, size % width));
         }
         return size / width;
     }
@@ -57,6 +55,6 @@ class Assert {
     }
 
     static UnsupportedOperationException throwUnsupportedOperation(String name, Class<?> clazz) {
-        return new UnsupportedOperationException(asFormat("Cannot suppprt %s from %s", name, clazz.getSimpleName()));
+        return new UnsupportedOperationException(String.format("Cannot suppprt %s from %s", name, clazz.getSimpleName()));
     }
 }
