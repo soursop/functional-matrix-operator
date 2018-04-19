1a35dc7aaa135b9a65b4346991953867867d005f
1002707 <stepping1st@gmail.com>
2018-04-19 13:40:27 +0900

    chore add gzip file read

--- src/test/java/com/github/soursop/matrix/operator/utils/Utils.java
+++ src/test/java/com/github/soursop/matrix/operator/utils/Utils.java
@@ -1,14 +1,60 @@
 package com.github.soursop.matrix.operator.utils;
 
 import com.github.soursop.matrix.operator.io.TextDoubleWritable;
+import com.google.common.base.Charsets;
+import com.google.common.base.Splitter;
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
 
 public class Utils {
 
     public static DataInput from(String resource) throws IOException {
-        URL url = com.google.common.io.Resources.getResource(resource);
-        return new DataInputStream(url.openStream());
+        return stream(asUrl(resource));
+    }
+
+    public static URL asUrl(String resource) throws IOException {
+        return com.google.common.io.Resources.getResource(resource);
+    }
+
+    public static DataInput stream(URL url) throws IOException {
+        if (url.getFile().endsWith("gz")) {
+            InputStream gzipStream = new GZIPInputStream(url.openStream());
+            Reader decoder = new InputStreamReader(gzipStream, StandardCharsets.UTF_8);
+            BufferedReader buffered = new BufferedReader(decoder);
+            InputStream targetStream =
+                    IOUtils.toInputStream(IOUtils.toString(buffered), Charsets.UTF_8);
+            return new DataInputStream(targetStream);
+        } else {
+            return new DataInputStream(url.openStream());
+        }
+    }
+
+    public static double[] gzip(String resource) throws IOException {
+        return gzip(asUrl(resource));
+    }
+
+    public static double[] gzip(URL url) throws IOException {
+        TextDoubleWritable.DoubleBuffer buffer = new TextDoubleWritable.DoubleBuffer();
+
+        InputStream gzipStream = new GZIPInputStream(url.openStream());
+        Reader decoder = new InputStreamReader(gzipStream, StandardCharsets.UTF_8);
+        BufferedReader buffered = new BufferedReader(decoder);
+
+        String line;
+        while ((line = buffered.readLine()) != null) {
+            Iterable<String> split = Splitter.on(",").split(line);
+            for (String s : split) {
+                buffer.add(Double.parseDouble(s));
+            }
+        }
+        buffered.close();
+        gzipStream.close();
+
+        return buffer.values();
     }
 
     public static double[] read(String resource) throws IOException {
@@ -23,7 +69,7 @@ public class Utils {
         return writable.getValues();
     }
 
-    public static void print(String format, Object ... objects) {
+    public static void print(String format, Object... objects) {
         System.out.println(String.format(format, objects));
     }
 }
