e4cce9f750e67966a90aaec46fa6e689922230c3
1002707 <stepping1st@gmail.com>
2018-04-23 10:14:41 +0900

    chore change loop variable

--- src/main/java/com/github/soursop/matrix/operator/NextDoubleMatrix.java
+++ src/main/java/com/github/soursop/matrix/operator/NextDoubleMatrix.java
@@ -11,22 +11,20 @@ public class NextDoubleMatrix extends DenseDoubleMatrix {
     protected static WithValues combine(DoubleMatrix... matrices) {
         int width = 0;
         int height = 0;
-        int[] range = new int[matrices.length];
         for(int i = 0; i < matrices.length; i++) {
             DoubleMatrix matrix = matrices[i];
-            if (i + 1 < matrices.length) {
-                range[i + 1] = range[i] + matrix.width();
-            }
             width = width + matrix.width();
             height = Assert.assertSameHeightExceptZero(height, matrix.height());
         }
         double[] values = new double[height * width];
-        for(int w = 0; w < matrices.length; w++) {
-            DoubleMatrix matrix = matrices[w];
+        int to = 0;
+        for(int m = 0; m < matrices.length; m++) {
+            DoubleMatrix matrix = matrices[m];
             for(int h = 0; h < height; h++) {
                 double[] value = matrix.row(h);
-                System.arraycopy(value, 0, values, range[w] + h * width, value.length);
+                System.arraycopy(value, 0, values, to + h * width, value.length);
             }
+            to = to + matrix.width();
         }
         return new WithValues(width, values);
     }
