b07979b5e7d2a776e96cd136b7e01f458da90e5a
stepping1st <stepping1st@gmail.com>
2018-05-01 21:55:34 +0900

    test add transpose case

--- src/test/java/com/github/soursop/matrix/operator/OperatorsTest.java
+++ src/test/java/com/github/soursop/matrix/operator/OperatorsTest.java
@@ -13,12 +13,13 @@ import static org.hamcrest.core.Is.is;
  * @created 2018. 4. 3.
  */
 public class OperatorsTest {
-    private double[] sample = new double[]{ 1d, 2d, 3d, 4d, 5d, 6d };
+    private double[] before = new double[]{ 1d, 2d, 3d, 4d, 5d, 6d };
+    private double[] after = new double[]{ 7d, 8d, 9d, 10d, 11d, 12d };
     private double[] transpose = new double[]{ 2d, 4d, 6d, 3d, 5d, 7d };
 
     @Test
     public void testProductWithMatrix() {
-        DoubleMatrix one = DenseDoubleMatrix.of(2, sample);
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
         DoubleMatrix other = DenseDoubleMatrix.of(3, transpose);
         DoubleMatrix result = one.product(other).invoke();
         assertThat(asList(result.values()), is(asList(
@@ -36,7 +37,7 @@ public class OperatorsTest {
 
     @Test
     public void testMultiplyWithMatrix() {
-        DoubleMatrix one = DenseDoubleMatrix.of(2, sample);
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.multiply(one).invoke();
         assertThat(asList(result.values()), is(asList(
                 1d * 1d
@@ -50,8 +51,8 @@ public class OperatorsTest {
 
     @Test
     public void testPlusWithMatrix() {
-        DoubleMatrix one = DenseDoubleMatrix.of(2, sample);
-        DoubleMatrix other = DenseDoubleMatrix.of(2, sample);
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
+        DoubleMatrix other = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.plus(other).invoke();
         assertThat(asList(result.values()), is(asList(
                 1d + 1d
@@ -65,8 +66,8 @@ public class OperatorsTest {
 
     @Test
     public void testMinusWithMatrix() {
-        DoubleMatrix one = DenseDoubleMatrix.of(2, sample);
-        DoubleMatrix other = DenseDoubleMatrix.of(2, sample);
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
+        DoubleMatrix other = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.minus(other).invoke();
         assertThat(asList(result.values()), is(asList(
                 1d - 1d
@@ -80,7 +81,7 @@ public class OperatorsTest {
 
     @Test
     public void testDivideWithOperator() {
-        DoubleMatrix one = DenseDoubleMatrix.of(2, sample);
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.divide(new DoubleOperator(2)).invoke();
         assertThat(asList(result.values()), is(asList(
                 1d / 2d
@@ -94,7 +95,7 @@ public class OperatorsTest {
 
     @Test
     public void testMinusWithIdentity() {
-        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, sample);
+        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.minus();
         assertThat(asList(result.values()), is(asList(
                 -1d
@@ -108,7 +109,7 @@ public class OperatorsTest {
 
     @Test
     public void testDivideWithIdentity() {
-        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, sample);
+        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, before);
         DoubleMatrix result = one.divide();
         assertThat(asList(result.values()), is(asList(
                 1/1d
@@ -120,11 +121,31 @@ public class OperatorsTest {
         )));
     }
 
+    @Test
+    public void testTranspose() {
+        DoubleMatrix one = DenseDoubleMatrix.of(2, before);
+        DoubleMatrix other = DenseDoubleMatrix.of(2, after);
+        DoubleMatrix result = one.transpose().product(other).invoke();
+        assertThat(asList(result.values()), is(asList(
+                1d * 7d + 3d * 9d + 5d * 11d
+                , 1d * 8d + 3d * 10d + 5d * 12d
+                , 2d * 7d + 4d * 9d + 6d * 11d
+                , 2d * 8d + 4d * 10d + 6d * 12d
+        )));
+        DoubleMatrix transpose = one.transpose().product(other).transpose().invoke();
+        assertThat(asList(transpose.values()), is(asList(
+                1d * 7d + 3d * 9d + 5d * 11d
+                , 2d * 7d + 4d * 9d + 6d * 11d
+                , 1d * 8d + 3d * 10d + 5d * 12d
+                , 2d * 8d + 4d * 10d + 6d * 12d
+        )));
+    }
+
     @Ignore
     @Test
     public void testAsSimple() {
-        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, sample);
-        DenseDoubleMatrix other = DenseDoubleMatrix.of(2, sample);
+        DenseDoubleMatrix one = DenseDoubleMatrix.of(2, before);
+        DenseDoubleMatrix other = DenseDoubleMatrix.of(2, before);
         Plus minus = one.divide().minus(other).plus(other);
         System.out.println(minus.asSimple(0));
     }
